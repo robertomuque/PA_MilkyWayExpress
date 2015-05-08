63c60ecbadfb1ec52d0aa598f499bf694f50017f
Miguel Fernandes <miguel.dsf.91@gmail.com>
2015-05-08 18:01:08 +0100

    Interface

--- src/UI_Texto/ITexto.java
+++ src/UI_Texto/ITexto.java
@@ -1,18 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package UI_Texto;
-import Jogo.*;
 
-/**
- *
- * @author inose_000
- */
+package UI_Texto;
+
+import Jogo.*;
+import Cartas.*;
+
+
+
+
+
+
+
 public class ITexto {
-    int [][] matrizcartas = new int [10][10];
     Jogo jogo = new Jogo();
+    Carta [][] mapData;
+    int [][] matrizcartas = new int [10][10];
+    
+    public ITexto(){
+        mapData = jogo.getMapa();
+    }
     
     
     void adicionaJogador(){
