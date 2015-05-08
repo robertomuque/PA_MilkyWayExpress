510dd86f6c3353642c69da62f6d60344b6721bcd
Miguel Fernandes <miguel.dsf.91@gmail.com>
2015-05-08 18:24:48 +0100

    Inicio da preparacao da impressao do mapa Miguel

--- src/UI_Texto/ITexto.java
+++ src/UI_Texto/ITexto.java
@@ -3,6 +3,8 @@ package UI_Texto;
 
 import Jogo.*;
 import Cartas.*;
+import Planetas.*;
+
 
 
 
@@ -18,6 +20,38 @@ public class ITexto {
     public ITexto(){
         mapData = jogo.getMapa();
     }
+    
+    public void printMapa(){
+        int i, j;
+        int dimX = 10;
+        int dimY = 10;
+        char letra;
+        
+        for(i = 0; i < dimX; i++){
+            for(j = 0; j < dimY; j++){
+                if(mapData[i][j] == null){
+                    letra = ' ';
+                }
+                else if(mapData[i][j] instanceof EmptySpace){
+                    letra = 'E';
+                }
+                else if(mapData[i][j] instanceof WormHole){
+                    letra = 'W';
+                }
+                else if(mapData[i][j] instanceof Planeta){
+                    letra = 'P';
+                }
+                else if(mapData[i][j] instanceof PlanetaPirata){
+                    letra = 'K';
+                }
+                
+            }
+        }
+        
+        
+        
+    }
+    
     
     
     void adicionaJogador(){
